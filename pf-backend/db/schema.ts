d8739a3b6aa60dc7609020cfed3c53c9ce8fff42
Korn Sriwattana <korn_s@cmu.ac.th>
2025-08-05 22:56:36 +0700

    update schema in db,backend

--- pf-backend/db/schema.ts
+++ pf-backend/db/schema.ts
@@ -11,14 +11,22 @@ export const todoTable = pgTable("todo", {
   todoText: varchar("todo_text", { length: 255 }).notNull(),
   isDone: boolean("is_done").default(false),
   tagId: uuid("tag_id").references(() => tagTable.id),
+  ownerId: uuid("owner_id").references(() => userTable.id),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at", { mode: "date", precision: 3 }).$onUpdate(
     () => new Date()
   ),
-  dueDate: timestamp("due_date", { mode: "date" }), 
+  dueDate: timestamp("due_date", { mode: "date" }),
 });
 
 export const tagTable = pgTable("tag", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: varchar("name", { length: 100 }).notNull().unique(),
+});
+
+export const userTable = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  name: varchar("name", { length: 255 }),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
 });
